7a499c7e7e9900b1939fb3278794ba02addaed8b
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-11-21 10:06:55 +0100

    exercise done

---
+++ esercizio-react/esercizio-componenti/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react'
+import{HelloWorld} from './HelloWorld'
+
+const helloWorldElement = <HelloWorld/>
+
+ReactDOM.render(helloWorldElement, document.querySelector('#root'))

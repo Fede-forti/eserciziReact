cccd96f33503b2b5974968e41046940ea9cea796
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-11-25 15:40:50 +0100

    exercise done

--- esercizio-react/esercizioState/state-app/src/Counter.js
+++ esercizio-react/esercizioState/state-app/src/Counter.js
@@ -1,32 +1,12 @@
 import React from "react";
-import { CounterDisplay } from "./CounterDisplay";
+import { ClickCounter } from "./ClickCounter";
 
 export class Counter extends React.Component {
-  state = {
-    count: this.props.initialValue,
-  };
-
-  componentDidMount() {
-    
-    setInterval(() => {
-      this.setState((state) => {
-        return {
-          count: state.count + this.props.incrementBy,
-        };
-      });
-    }, this.props.timeout);
-  }
   render() {
     return (
       <div>
-        <CounterDisplay counter={this.state.count} />
+        <ClickCounter counter={this.props.count} />
       </div>
     );
   }
 }
-
-Counter.defaultProps = {
-  initialValue: 0,
-  incrementBy: 1,
-  timeout: 1000,
-};

b08c82175c1c248684ac84ee55bac952296396e6
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-11-29 18:34:56 +0100

    exercise done

--- esercizio-react/esercizioState/state-app/src/InteractiveWelcome.js
+++ esercizio-react/esercizioState/state-app/src/InteractiveWelcome.js
@@ -1,44 +1,12 @@
 import React from 'react'
-import { Welcome } from './Welcome'
+import { Login } from './Login'
 
-export class InteractiveWelcome extends React.Component {
-
-    state = {
-        username : '',
-        password : '',
-        remember : false
-    }
-
-    formInputChange = (event) => {
-
-        const value = event.target.value
-        const name = event.target.name
-
-        this.setState({
-            [name] : value,
-        })
-        
-    }
-
-    handleCheckboxChange = (event) => {
-        const name = event.target.name
-        const checked = event.target.checked
-
-        this.setState({
-            [name] : checked
-        })
-    }
-
-    
-
+export class InteractiveWelcome extends React.Component {   
 
     render() {
         return (
             <div>
-                <Welcome name={this.state.username} />
-                <input name='username' value = {this.state.username} onChange={this.formInputChange}></input>
-                <input name='password' type= 'password' value = {this.state.password} onChange={this.formInputChange}></input>
-                <input name='remember' type='checkbox' checked = {this.state.remember} onChange={this.handleCheckboxChange}></input>
+                <Login name={this.props.name}/>
             </div>
         )
     }
---
+++ esercizio-react/esercizioState/state-app/src/Login.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export class Login extends React.Component {
+    state = {
+        username : '',
+        password : '',
+        remember : false
+    }
+
+    formInputChange = (event) => {
+
+        const value = event.target.value
+        const name = event.target.name
+
+        this.setState({
+            [name] : value,
+        })
+        
+    }
+
+    handleCheckboxChange = (event) => {
+        const name = event.target.name
+        const checked = event.target.checked
+
+        this.setState({
+            [name] : checked
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <h1> Welcome {this.state.username} </h1>
+                <input name='username' value = {this.state.username} onChange={this.formInputChange}></input>
+                <input name='password' type= 'password' value = {this.state.password} onChange={this.formInputChange}></input>
+                <input name='remember' type='checkbox' checked = {this.state.remember} onChange={this.handleCheckboxChange}></input>
+            </div>
+        )
+    }
+
+}

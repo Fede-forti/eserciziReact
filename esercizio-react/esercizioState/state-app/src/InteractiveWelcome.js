091836fbcaf8bfd657a74f73910c8cb20149b73c
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-11-29 22:08:28 +0100

    exercise done

--- esercizio-react/esercizioState/state-app/src/InteractiveWelcome.js
+++ esercizio-react/esercizioState/state-app/src/InteractiveWelcome.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Login } from './Login'
+import { UncontrolledLogin } from './UncontrolledLogin'
 
 export class InteractiveWelcome extends React.Component {   
 
     render() {
         return (
             <div>
-                <Login/>
+                <UncontrolledLogin/>
             </div>
         )
     }

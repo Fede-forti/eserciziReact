134eb9a7d6f2dd677b483d00e3220dd624f6f366
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-12-03 13:46:35 +0100

    exercise done

--- esercizio-react/esercizioState/state-app/src/GithubUser.js
+++ esercizio-react/esercizioState/state-app/src/GithubUser.js
@@ -1,26 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-export function Githubuser ({ username }) {
+export function Githubuser({ username }) {
+  const [data, setData] = useState();
 
-const [data, setData]= useState()
+  async function fetchGithubUser(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    const json = await response.json();
+    console.log(json)
+    setData(json);
+  }
 
+  useEffect(() => {
+    fetchGithubUser(username)
+  }, [username])
 
-
-    useEffect(() => {
-        fetch(`https://api.github.com/users/${username}`)
-        .then(response => {
-            return response.json()
-        })
-        .then(json => {
-            console.log(json)
-            setData(json)
-        })
-    }, [username])
-
-    return (
-        <div>
-            { data && <h1>{data.login}</h1>}
-        </div>
-    )
+  return <div>{data && <h1>{data.login}</h1>}</div>;
 }

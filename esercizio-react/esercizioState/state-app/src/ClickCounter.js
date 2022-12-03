53a27569a64281036beb93368e24bc5528dd6c0e
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-12-03 16:12:27 +0100

    exercise done

--- esercizio-react/esercizioState/state-app/src/ClickCounter.js
+++ esercizio-react/esercizioState/state-app/src/ClickCounter.js
@@ -1,30 +1,5 @@
-import React, { useState } from "react";
-
-function useCounter(initialValue = 0) {
-  const [counter, setCounter] = useState(initialValue)
-
-  function handleCounterIncrement() {
-    setCounter((c) => {
-      return c + 1});
-  }
-
-  function handleCounterDecrement() {
-    setCounter((c) => {
-      return c - 1});
-  }
-
-  function handleCounterReset() {
-    setCounter(initialValue)
-  }
-
-  return {
-    counter: counter,
-    onIncrement: handleCounterIncrement,
-    onDecrement: handleCounterDecrement,
-    onReset: handleCounterReset
-  }
-
-}
+import React from "react";
+import { useCounter } from "./useCounter";
 
 export function ClickCounter({ initialValue = 0 }) {
 const {counter, onIncrement, onDecrement, onReset} = useCounter(initialValue)  

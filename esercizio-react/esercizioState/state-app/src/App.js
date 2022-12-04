dc6ea25a2eafd816fa43fc6b2c4ac8f09c4e994f
Fede-forti <108474540+Fede-forti@users.noreply.github.com>
2022-12-04 15:08:05 +0100

    exercise done

--- esercizio-react/esercizioState/state-app/src/App.js
+++ esercizio-react/esercizioState/state-app/src/App.js
@@ -1,12 +1,23 @@
-import React from "react"; 
-import { CarDetails } from "./CarDetails";
+import React, { useState } from "react"; 
+import { DisplayLanguage } from "./DisplayLanguage";
+import { LanguageContext } from "./LanguageContext";
 
-export class App extends React.Component {
-    render() {
+export function App() {
+   const [language, setLanguage] = useState('en')
+
+   function handleChangeLanguage(event) {
+    setLanguage(event.target.value)
+   }
+
         return (
             <div>
-                <CarDetails initialData={{ model: "Fiat Panda", year: "2007", color: "Grey" }}/>
+                <select value={language} onChange={handleChangeLanguage}>
+                    <option value='en'>English</option>
+                    <option value='it'>Italiano</option>
+                </select>
+                <LanguageContext.Provider value={language}>
+                    <DisplayLanguage/>
+                </LanguageContext.Provider>
             </div>
         )
     }
-}
---
+++ esercizio-react/esercizioState/state-app/src/DisplayLanguage.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { LanguageContext } from "./LanguageContext";
+
+export function DisplayLanguage(){
+    const language = useContext(LanguageContext)
+
+    return (
+            <h3>Current language: {language}</h3>
+        
+    )
+}
